43d74252bdf20b3e7ea76fb0e62d6c688d166e5f
Nenoeglah <134705870+Nenoeglah@users.noreply.github.com>
2023-06-30 00:31:50 +0300

    completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,47 @@
-// Add your code here
+function submitData(name, email) {
+  fetch("http://localhost:3000/users" ,{
+    method: "POST",
+    headers: {"Content-Type":"application/json",
+    Accept: "application/json",
+    },
+    body:JSON.stringify({
+      name: name,
+      email: email,
+
+    }),
+  })
+  .then(res =>res.json())
+  .then(data =>{
+
+    appendChildDOM(data)
+
+    console.log(data)
+  })
+  .catch(errorMessage=>{
+    document.body.innerHTML +=errorMessage;
+
+
+  })
+}
+const form = document.querySelector('#form')
+console.log(form)
+form.addEventListener("submit", e => {
+  e.preventDefault()
+  
+  
+  const name = document.getElementById('name').value
+  const email = document.getElementById('email').value
+  submitData(name, email)
+})
+
+function appendChildDOM(data){
+  for (let i = 0; i < data.length; i++){
+  const id = data.id
+  const ptag = document.createElement('p')
+  const div = document.querySelector('.append')
+  div.innerHTML = ptag
+}
+}
+
+
+ 
